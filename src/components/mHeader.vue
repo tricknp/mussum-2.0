efb91275828ae7f1bcf7fbbcb5d104dae1ce667d
Patrick <np_patrick@yahoo.com>
2018-03-18 05:46:44 -0300

    menu with teachers added

--- src/components/mHeader.vue
+++ src/components/mHeader.vue
@@ -11,27 +11,57 @@
         <img src="#" alt="">
       </div>
 
-      <ul class="head-ul">
-        <li>Professoris</li>
-        <li>Horáris</li>
-        <li>{{repolho}}</li>
-      </ul>
+      <div class="list-links">
+
+        <div class="text-xs-left" id="">
+          <v-menu open-on-hover top offset-y>
+            <v-btn id="btn-list" dark slot="activator">Professoris</v-btn>
+            <v-list>
+              <v-list-tile v-for="professor in professores" :key="professores.name" @click="">
+                <v-list-tile-title>{{ professor.name }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+          
+          <v-menu open-on-hover top offset-y>
+            <v-btn id="btn-list" dark slot="activator"><a href="#" target="_blank">Horaris</a></v-btn>
+          </v-menu>
+        </div>   
+
+      </div>
 
       <img class="menu-bar" src="../assets/images/menu-bar.svg">
     </nav>
-
+    
   </header>
 
 </template>
 
 
 <script>
+
+const linkHorarios = 'https://docs.google.com/spreadsheets/d/1eh4vz5QlTYqHdMzX36_a_UF6xhJ5HvazfKGMiBt_zeU/edit?usp=sharing';
+
 export default {
   name: 'mHeader',
 
 data(){
   return{
-    repolho: 'repolhinho',
+
+    horarios: linkHorarios,
+
+    professores: 
+    [
+      {name: 'Angelo'},
+      {name: 'Edecio'},
+      {name: 'Gladimir'},
+      {name: 'CV'},
+      {name: 'Dartagnan'},
+    ],
+
+   
+
+    
   };
 },
 
