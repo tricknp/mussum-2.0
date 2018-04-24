46de3019184f24b30f6c3f97f6791a8186c340a2
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-04-24 20:43:34 -0300

    Start work at Login - Send through AJAXAR request the user info from back to validate

--- src/components/Authentication/Login.vue
+++ src/components/Authentication/Login.vue
@@ -1,19 +1,58 @@
+import { Script } from "vm";
+
 <template>
   <div>
-      <Header />
-      <FormLogin />
+    <Header />
+    <FormLogin />
+    <form>
+      <div class="form-group"></div>
+      <label for="username">Username</label>
+      <input type="text" id="username" name="username" class="form-control" v-model="username">
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" class="form-control" v-model="password">
+      </div>
+      <div>
+        <button type="submit" @click.prevent="login">Login</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import Header from '../FixedComponents/Header/Header'
-import FormLogin from './Views/FormLogin'
+  import axios from 'axios';
+  import Header from '../FixedComponents/Header/Header'
+  import FormLogin from './Views/FormLogin'
 
-export default {
-  name: 'Login',
+  export default {
+    name: 'Login',
+    components: { Header, FormLogin },
 
-  components: { Header, FormLogin  }
+    data() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
 
-}
+    methods: {
+      login() {
+        axios.post('http://localhost:8080/login', //(Need to create a fake server to test) Still there is no Post for login route.
+          { username: this.username, password: this.password }, //Send the user unformation from back to validate
+          { headers: { 'X-Requested-With': 'XMLHttpRequest' } }) //Send the information through an AJAX request
+          .then(
+            (response) => {
+              const token = response.data.token;
+              const base64Url = token.split('.')[1];
+              const base64 = base64Url.replace('-', '+').replace('_', '/');
+              localStorage.setItem('token', token); //Store the token to send it to the back whem an access is needed
+            }
+          )
+          .catch(
+            (error) => console.log(error)
+          );
+      }
+    }
+  }
+
 </script>
-
