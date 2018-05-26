ea5610420304041ed90be48edacb4078cd29fda0
ynb <yurinbento@hotmail.com>
2018-05-26 12:12:50 -0300

    token nos requests middleware

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Admin from '@/components/SuperUsers/Admin/Admin'
 import Teacher from '@/components/SuperUsers/Teacher/Teacher'
 import NotFound from '@/components/GeneralViews/NotFoundPage'
 import AuthMiddleware from '../services/middleware'
+import AxiosMid from '../services/axiosMid'
 
 
 Vue.use(Router)
