bbc158aacec6f9ead702992538920c5d27c589cf
Yuri Bento <yurinbento@hotmail.com>
2018-06-17 19:56:24 -0300

    feed link fixed

--- src/components/GeneralViews/Feed/Feed.vue
+++ src/components/GeneralViews/Feed/Feed.vue
@@ -28,6 +28,14 @@
                         <p> {{ content.dataCriacao }} </p>
                     </div>
                 
+                    <div v-if="content.tipo == 'link'">
+                        <img src="#">
+                        <p> <b>{{ content.professor }}</b> {{ textLink }} </p>
+                        <p> <a :href="content.link">{{ `${content.titulo}` }}</a> </p>
+                        <p> {{ `${content.comentario}` }}  </p>
+                        <p> {{ content.dataCriacao }} </p>
+                    </div>
+                
                 </div>
             </div>
         </div>
@@ -35,61 +43,71 @@
 </template>
 
 <script>
-import axios from 'axios'
-import {url} from '../../_mixins/url'
-import LoginVue from '../../Authentication/Login.vue';
+import axios from "axios";
+import { url } from "../../_mixins/url";
+import LoginVue from "../../Authentication/Login.vue";
 
 export default {
+  data() {
+    return {
+      feedContent: "",
+      textUpload: "adicionou um novo arquivo em",
+      textRecado: "adicionou um novo recado.",
+      textLink: "adicionou um novo link."
+    };
+  },
 
-    data(){
-        return{
-            feedContent: '',
-            textUpload: 'adicionou um novo arquivo em',
-            textRecado: 'adicionou um novo recado.'
-        }
-    },
+  mixins: [url],
 
-    mixins: [ url ],
+  created() {
+    this.feed();
+  },
 
-    created(){
-        this.feed()
-    },
+  methods: {
+    feed() {
+      axios.get(`${this.BASE_URL}api/feed`).then(res => {
+        let contents = res.data;
+        let a = [];
 
-    methods:{
-        feed(){
-            axios.get(`${this.BASE_URL}api/feed`).then( res =>  {
-                let contents = res.data
-                let a = []   
+        contents.forEach(element => {
+          console.log(element);
+          if (element.tipo == "upload") {
+            a.unshift({
+              tipo: "upload",
+              professor: element.professor,
+              dir: element.dir,
+              arquivo: element.arquivo,
+              comentario: element.comentario,
+              dataCriacao: element.dataCriacao
+            });
+          }
+          if (element.tipo == "recado") {
+            a.unshift({
+              tipo: "recado",
+              professor: element.professor,
+              titulo: element.titulo,
+              comentario: element.comentario,
+              dataCriacao: element.dataCriacao
+            });
+          }
+          if (element.tipo == "link") {
+            a.unshift({
+              tipo: "link",
+              professor: element.professor,
+              titulo: element.titulo,
+              comentario: element.comentario,
+              link: element.link,
+              dataCriacao: element.dataCriacao
+            });
+          }
+        });
 
-                contents.forEach(element => {
-                    console.log(element)
-                    if (element.tipo == 'upload') {          
-                    a.unshift({
-                          tipo: 'upload',
-                          professor: element.professor,
-                          dir: element.dir,
-                          arquivo: element.arquivo,
-                          comentario: element.comentario,
-                          dataCriacao: element.dataCriacao
-                      })
-                    }else if(element.tipo == 'recado'){
-                        a.unshift({
-                          tipo: 'recado',
-                          professor: element.professor,
-                          titulo: element.titulo,
-                          comentario: element.comentario,
-                          dataCriacao: element.dataCriacao
-                      })
-                    }
-                });
-
-                this.feedContent = a;   
-                console.log(this.feedContent)
-            })
-        }
+        this.feedContent = a;
+        console.log(this.feedContent);
+      });
     }
-
-}
+  }
+};
 </script>
 
 
