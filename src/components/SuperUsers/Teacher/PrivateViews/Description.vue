012a57ed29f814f79b127efbe8f092f096b8850e
Yuri Bento <yurinbento@hotmail.com>
2018-06-25 09:29:03 -0300

    merge

--- src/components/SuperUsers/Teacher/PrivateViews/Description.vue
+++ src/components/SuperUsers/Teacher/PrivateViews/Description.vue
@@ -3,41 +3,41 @@
     <h1> {{ `${this.name} ${this.lastname}` }} </h1>
     <div class="div-desc-teacher">
           <IconResume />
-          <input type="text" 
+          <input type="text"
                 :disabled="disabled"
-                placeholder="Sem descrição" 
+                placeholder="Sem descrição"
                 class="input-teacher-profile"
                 ref="desc"
-                v-model="this.description"> 
+                v-model="this.description">
           <button type="submit" @click="actionDesc" v-if="!editFocused">
             <IconEdit />
-          </button> 
-          
+          </button>
+
           <button type="submit" @click.prevent="onSubmit" v-else>
             <IconOk />
-          </button> 
+          </button>
     </div>
-    
+
     <div class="div-email-teacher">
       <IconEmail />
-      <input type="text"  
+      <input type="text"
              :disabled="disabled"
-             placeholder="Nenhum e-mail informado" 
+             placeholder="Nenhum e-mail informado"
              class="input-teacher-profile"
              ref="mail"
-             v-model="this.email"> 
-    
+             v-model="this.email">
+
       <button type="submit" @click="actionMail" v-if="!mailFocused">
         <IconEdit />
-      </button> 
+      </button>
 
       <button type="submit" @click.prevent="onSubmit" v-else>
         <IconOk />
-      </button> 
+      </button>
     </div>
 
 
-  </div>  
+  </div>
 </template>
 
 
